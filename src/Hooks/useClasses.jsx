56c07bfb922db0791b31253bb4cbbb96d3b3b2f6
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-06-09 21:35:35 +0600

    Created Classes Hook

--- src/Hooks/useClasses.jsx
+++ src/Hooks/useClasses.jsx
@@ -1,11 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
 
 const useClasses = () => {
 
-    return (
-        <div>
-            
-        </div>
-    );
+    const { data: classes = [], isLoading: loading, refetch } = useQuery({
+        queryKey: ['classes'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/classes')
+            return res.json();
+        }
+    })
+    return [classes, loading, refetch]
 };
 
 export default useClasses;
