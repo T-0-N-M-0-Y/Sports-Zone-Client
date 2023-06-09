d21aa7c4f03a1fc0d0ede4bf956f1e59ade0ad9c
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-06-09 21:29:40 +0600

    Instal query

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,13 @@ import Authprovider from './Provider/AuthProvider.jsx';
 import Login from './Components/LoginAndSignUpPage/Login.jsx';
 import SignUp from './Components/LoginAndSignUpPage/SignUp.jsx';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -40,9 +47,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <HelmetProvider>
         <Authprovider>
-          <div>
-            <RouterProvider router={router} />
-          </div>
+          <QueryClientProvider client={queryClient}>
+            <div>
+              <RouterProvider router={router} />
+            </div>
+          </QueryClientProvider>
         </Authprovider>
       </HelmetProvider>
     </React.StrictMode>,
