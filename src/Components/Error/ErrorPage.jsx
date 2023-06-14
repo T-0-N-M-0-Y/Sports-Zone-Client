fa59a6c613bd813cc82b919463bdc6231aaa069d
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-06-14 16:58:09 +0600

    Error Page

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -10,7 +10,25 @@ const ErrorPage = () => {
             <div className="card w-full h-screen image-full text-center">
                 <figure><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJJ_UoLAPR8wkbi2QJnYPtOMdvSNtghsh4w2sGsfZLa-o4_fRujCT5zwGQXXHeFsTrmKg&usqp=CAU" alt="Shoes" /></figure>
                 <div className="card-body">
-                    <p className='text-3xl text-white font-semibold font-serif flex justify-center items-center'>Error: {status} <br /> {error.message}</p>
+                    <motion.div
+                        className="box"
+                        animate={{
+                            scale: [1, 2, 2, 1, 1],
+                            rotate: [0, 0, 360, 360, 0],
+                            borderRadius: ["0%", "0%", "50%", "50%", "0%"]
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 0.2, 0.5, 0.8, 1],
+                            repeat: Infinity,
+                            repeatDelay: 1
+                        }}
+                    >
+                        <p className='text-3xl text-red-500 font-semibold font-serif flex justify-center mt-44'>Error: {status} </p>
+                    </motion.div>
+                    <br />
+                    <p className='text-3xl text-white font-semibold font-serif flex justify-center items-center'>{error.message}</p>
                     <div className="card-actions justify-center items-center">
                         <motion.div className="box"
                             whileHover={{ scale: 2 }}
