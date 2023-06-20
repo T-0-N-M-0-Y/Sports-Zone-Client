af82a5932522c5e953cc63c178b7a06fd4f34243
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-06-21 03:52:12 +0600

    Update

--- src/Components/AllInstructors/InstructorsDetails.jsx
+++ src/Components/AllInstructors/InstructorsDetails.jsx
@@ -6,8 +6,6 @@ const InstructorsDetails = () => {
 
     const [instructors] = useInstructors();
 
-    const sortedInstructors = instructors.sort((a, b) => b.numStudents - a.numStudents);
-
     return (
         <div>
             <Helmet>
@@ -16,7 +14,7 @@ const InstructorsDetails = () => {
             <h1 className='md:text-4xl text-2xl uppercase font-bold pt-32 pb-10 text-center'>Know Our Instructors</h1>
             <div className="md:grid grid-cols-3 gap-5 mx-5 md:mx-20">
                 {
-                    sortedInstructors.map(instructor =>
+                    instructors.map(instructor =>
                         <AllInstructors
                             key={instructor._id}
                             instructor={instructor}
--- src/Hooks/useInstructors.jsx
+++ src/Hooks/useInstructors.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const useInstructors = () => {
 
-    const { data: instructors = [], refetch } = useQuery({
+    const { data: instructors = [], refetch} = useQuery({
         queryKey: ['instructors'],
         queryFn: async () => {
             const res = await fetch('https://assignment-12-sports-academies-server-site-t-0-n-m-0-y.vercel.app/instructors')
